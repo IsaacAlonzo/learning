cdaaba8b3050318ca13c384d4ffc191c25c1e2d3
IsaacAlonzo <isaac.alonzo@icloud.com>
2019-10-02 15:41:37 -0500

    added comments explaining the floating points and their importance

--- learning.py
+++ learning.py
@@ -25,3 +25,21 @@ print(3.0 + 0.5)
 print("%.2f" % 3.5)
 #by inputing this command I now made my equation go to 2 decimal places in order to make it more precise
 #This way I can make sure that my equation is precise for someone who needs it
+
+
+# variables
+cars = 100
+space_in_a_car=4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers/cars_driven
+
+print("There are" ,cars, "cars available")
+print("there are only" ,drivers, "drivers available")
+print("there will b" ,cars_not_driven, "people today")
+print("We can transport" , carpool_capacity, "people today")
+print("We have" ,passengers, "to carpool today")
+print("We need to put about" ,average_passengers_per_car, "in each car")
