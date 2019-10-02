4327273f49781c2cd4ccdf7b3bc27022232eb7b4
IsaacAlonzo <isaac.alonzo@icloud.com>
2019-10-02 15:42:30 -0500

    added variables

--- learning.py
+++ learning.py
@@ -38,8 +38,8 @@ carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers/cars_driven
 
 print("There are" ,cars, "cars available")
-print("there are only" ,drivers, "drivers available")
-print("there will b" ,cars_not_driven, "people today")
+print("There are only" ,drivers, "drivers available")
+print("There will b" ,cars_not_driven, "people today")
 print("We can transport" , carpool_capacity, "people today")
 print("We have" ,passengers, "to carpool today")
 print("We need to put about" ,average_passengers_per_car, "in each car")
